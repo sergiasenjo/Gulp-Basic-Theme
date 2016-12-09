7683a57cd672260fb6418e8289e36022cedb632a
Sergio Asenjo <sergio.asenjo@novaquality.es>
2016-12-09 13:48:23 +0100

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
-  gulp.watch('app/less/*.less', ['less']);
-  gulp.watch('app/less/*.less', ['minify-css']);
+gulp.task('watch', ['less', 'minify-css', 'browserSync'], function(){
+  gulp.watch('app/less/*.less', ['less', 'minify-css', browserSync.reload]);
+  gulp.watch('app/*.html', browserSync.reload);
 })
 
 gulp.task('less', function () {
@@ -13,11 +14,25 @@ gulp.task('less', function () {
         .pipe(less({
           paths: [ path.join(__dirname, 'less', 'includes') ]
         }))
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('minify-css', function() {
+gulp.task('minify-css', ['less'], function() {
     return gulp.src('app/css/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('app/dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
+
+gulp.task('browserSync', ['less', 'minify-css'], function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
